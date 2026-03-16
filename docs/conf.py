0eb4c29be9bec728742a3973ddc38af277b908e8
europeandreams <europeandreams01@gmail.com>
2026-03-16 09:53:16 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = 'European Dreams - Study in Italy'
+author = 'European Dreams'
+release = '1.0'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+html_static_path = ['_static']
+
+html_favicon = '_static/favicon.png'
+
+html_title = "Study in Italy | European Dreams"
